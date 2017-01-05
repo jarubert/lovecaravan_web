8e4b0d7585ad97fee7a33a07f84b0c9940452dae
Jeferson Rubert <Jeferson.Rubert@kitandace.com>
2017-01-05 01:11:31 -0800

    normalized css, divided it into separade files, fixed header and footer issues with links

--- js/common.js
+++ js/common.js
@@ -1,15 +1,7 @@
 <script src="jquery-3.1.1.min.js"></script>
 <script> 
     $(document).ready(function(){
-    	var pathname = window.location.pathname;
-    	if((pathname.match(/lovecaravan_web\/$/) == null) || (pathname == "//")){
-    		$(".footer").load('../footer.html'); 
-      		$(".topNav").load('../topNav.html'); 		
-    	}else{
     		$(".footer").load('footer.html'); 
       		$(".topNav").load('topNav.html'); 
-    	}
-      
     });
     </script> 
-
