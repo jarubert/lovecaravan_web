717851215f351741ff4419e1f9ef00199216bdb0
Jeferson Rubert <Jeferson.Rubert@kitandace.com>
2017-01-05 01:43:32 -0800

    created styles for menus and added arrow scroll function

--- js/common.js
+++ js/common.js
@@ -1,7 +1,16 @@
 <script src="jquery-3.1.1.min.js"></script>
 <script> 
     $(document).ready(function(){
-    		$(".footer").load('footer.html'); 
-      		$(".topNav").load('topNav.html'); 
+    	$(".footer").load('footer.html'); 
+      	$(".topNav").load('topNav.html'); 
     });
-    </script> 
+
+    $(".arrow_down").click(function(){
+    	scrolled=600;
+        
+				$("body").animate({
+				        scrollTop:  scrolled
+				   });
+
+    })
+</script> 
